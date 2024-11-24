6d11b9d34f523a2a47594dff8f8d141b7fc2ca13
Lê Ngọc Anh <118795906+nanhctepy@users.noreply.github.com>
2024-11-24 14:24:14 +0700

    Create __init__.py

---
+++ zlapi/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+"""Zalo API for Python
+
+:Module write By Vex - Mod By Lê Ngọc Anh(nanh)
+"""
+from .models import *
+from ._client import ZaloAPI
+
+__title__ = "zlapi"
+__version__ = "1.0.1"
+__description__ = "Zalo API for Python"
+
+__copyright__ = "Copyright 2024 Module ZaloAPI By Lê Ngọc Anh"
+
+__author__ = "Lê Ngọc Anh"
+__email__ = "anhle4997@gmail.com"
+
+__all__ = ["ZaloAPI"]
